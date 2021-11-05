fa4b8daf56f2b96b32aa4dd834d5219dbe155ed2
Natalia <natalik3@mail.ru>
2021-11-05 18:35:50 +0300

    Homework lesson2

--- src/main/java/ru/balyanova/productData/Console.java
+++ src/main/java/ru/balyanova/productData/Console.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 @Configuration
@@ -14,47 +15,52 @@ public class Console {
 
         Scanner scanner = new Scanner(System.in);
         ProductRepository productRepository = context.getBean(ProductRepository.class);
-        // TODO: 05.11.2021  
+
         while (true) {
-            System.out.println("Create new cart? -Send '1' \n" +
-                    "If you want to exit - send '0'");
-            int createOrExit = scanner.nextInt();
-            if (createOrExit == 0) {
-                System.out.println("Exit");
-                break;
-            } else if (createOrExit == 1) {
+            System.out.println("Create new cart? - send '1' \n" +
+                               "If you want to exit - send '0'");
+            int createCartOrExit = scanner.nextInt();
+
+            if (createCartOrExit == 1) {
                 Cart cart = context.getBean(Cart.class);
-                System.out.println("Cart created. \n " +
-                        "Add product - send '1' \n " +
-                        "Delete product - send '2' \n" +
-                        "If you want to exit - send '0'");
-                actionWithCart(cart, scanner, productRepository);
-            } else break;
+                System.out.println("Cart created. \n " + stringAction());
+                try {
+                    actionWithCart(cart, scanner, productRepository);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else
+                System.out.println("Exit"); break;
         }
     }
+    private static String stringAction() {
+        return  "Add product - send '10' \n " +
+                "Delete product - send '20' \n" +
+                "If you want to exit - send '00'";
+    }
 
-    private static void actionWithCart(Cart cart, Scanner scanner, ProductRepository productRepository) {
+    private static void actionWithCart(Cart cart, Scanner scanner, ProductRepository productRepository) throws Exception{
         // TODO: 05.11.2021  
         while (true) {
             int scanActionWithCart = scanner.nextInt();
             switch (scanActionWithCart) {
-                case 0:
-                    System.out.println("Exit");
-                    break;
-                case 1:
-                    System.out.println("Choose the product by id: " + productRepository.getProductList());
+                case 10:
+                    System.out.println("Choose the product by id: \n" + productRepository.getProductList());
                     cart.addProduct(scanner.nextInt());
-                    System.out.println("Choose the action. " +
-                            "Add product - send '1' \n" +
-                            "Delete product - send '2' \n" +
-                            "If you want to exit - send '0'");
-                case 2:
+                    System.out.println("Choose the action. \n" + stringAction());
+                    break;
+                case 20:
+                    System.out.println("Choose the product by id: \n" + cart.getProductInCart());
                     cart.delProduct(scanner.nextInt());
                     System.out.println("Product deleted. Product in the cart: " + cart.getProductInCart());
-                    System.out.println("Choose the action. " +
-                            "Add product - send '1' \n" +
-                            "Delete product - send '2' \n" +
-                            "If you want to exit - send '0'");
+                    System.out.println("Choose the action. \n" + stringAction());
+                    break;
+                case 00:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("something wrong. \n" + stringAction());
+                    break;
             }
         }
     }
--- src/main/java/ru/balyanova/productData/Product.java
+++ src/main/java/ru/balyanova/productData/Product.java
@@ -18,6 +18,6 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product: " + id + " " + title + " " + cost + " руб.";
+        return "Product: " + id + " " + title + " " + cost + " руб. \n";
     }
 }
