d71aa187e3f104bf88453cd1c059571708dd9379
Natalia-Balyanova <75272081+Natalia-Balyanova@users.noreply.github.com>
2021-11-13 10:08:39 +0300

    change int to Long, delete action

--- src/main/java/ru/balyanova/productData/ProductRepository.java
+++ src/main/java/ru/balyanova/productData/ProductRepository.java
@@ -5,27 +5,28 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class ProductRepository {
     private List<Product> productList;
 
+    public List<Product> getProductList() {
+        return Collections.unmodifiableList(productList);
+    }
+
     @PostConstruct
     public void init() {
         productList = new ArrayList<>(Arrays.asList(
-                new Product(1, "Bread", 100),
-                new Product(2, "Bread Luxury", 200),
-                new Product(3, "Apple", 100),
-                new Product(4, "Milk", 70),
-                new Product(5, "Eggs", 100)
+                new Product(1L, "Bread", 100),
+                new Product(2L, "Bread Luxury", 200),
+                new Product(3L, "Apple", 100),
+                new Product(4L, "Milk", 70),
+                new Product(5L, "Eggs", 100)
         ));}
 
-    public List<Product> getProductList() {
-        return productList;
-    }
-
-    public Product findProductById(int id) {
-        return productList.stream().filter(i -> i.getId() == id).findFirst().get();
+    public Product findProductById(Long id) {
+        return productList.get(Math.toIntExact(id));
     }
 }
